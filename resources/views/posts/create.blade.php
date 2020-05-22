a3cea6929df4da65ffaf2255bf7813e295f2b5e5
Christian Beretta <beretta.christian2602@gmail.com>
2020-05-22 16:54:22 +0200

    organizzazione grafica con sass

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,66 +1,81 @@
-{{-- @dd($errors); --}}
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Create New Post</title>
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-</head>
-<body>
+@extends('layouts.layout')
+
+@section('title')
+    CREATE
+@endsection
+
+@section('header')
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <form  action="{{route('posts.store')}}" method="POST">
-                    @csrf {{-- token CRSF: cross-site request forgery attacks protector --}}
-                    @method('POST') {{--  hidden input --}}
-                    <div class="form-group">
-                        <label for="title">Post Title</label>
-                        <input type="text" class="form-control" name="title" placeholder="Post Title" value="{{ old('title') }}">
-                        @error('title')
-                        <div class="alert alert-danger"> {{ $message }} </div>
-                        @enderror
-                    </div>
-                    <div class="form-group">
-                        <label for="author">Post Author</label>
-                        <input type="text" class="form-control" name="author" placeholder="Post Author" value="{{ old('author') }}">
-                        @error('author')
-                        <div class="alert alert-danger"> {{ $message }} </div>
-                        @enderror
-                    </div>
-                    <div class="form-group">
-                        <label for="img">Post IMG</label>
-                        <input type="text" class="form-control" name="img" placeholder="Post IMG" value="{{ old('img') }}">
-                    </div>
-                    <div class="form-group">
-                        <label for="body">Textarea</label>
-                        <textarea class="form-control" name="body" rows="3">{{ (!empty(old('body'))) ? old('body') : 'Inserisci un testo' }}</textarea>
-                        @error('body')
-                        <div class="alert alert-danger"> {{ $message }} </div>
-                        @enderror
-                    </div>
-                    <div class="text-center">
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="published" id="inlineRadio1" value="1" {{ (old('published') == 1) ? 'checked' : '' }}>
-                            <label class="form-check-label" for="published">Published</label>
-                        </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="published" id="inlineRadio2" value="0" {{ (old('published') == 0) ? 'checked' : '' }}>
-                            <label class="form-check-label" for="unpublished">Unpublished</label>
-                        </div>
-                    </div>
-                    <div class="text-center">
-                        <button class="btn btn-primary" type="submit" value="salva">Submit form</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-12">
-                <a href="{{route('posts.index')}}" class="btn btn-light btn-lg btn-block active" role="button" aria-pressed="true">HOME</a>
+                <h1>Create New Post</h1>
             </div>
         </div>
     </div>
-</body>
-</html>
+@endsection
+
+@section('main')
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <form  action="{{route('posts.store')}}" method="POST">
+                        @csrf {{-- token CRSF: cross-site request forgery attacks protector --}}
+                        @method('POST') {{--  hidden input --}}
+                        <div class="form-group">
+                            <label for="title">Post Title</label>
+                            <input type="text" class="form-control" name="title" placeholder="Post Title" value="{{ old('title') }}">
+                            @error('title')
+                            <div class="alert alert-danger"> {{ $message }} </div>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="author">Post Author</label>
+                            <input type="text" class="form-control" name="author" placeholder="Post Author" value="{{ old('author') }}">
+                            @error('author')
+                            <div class="alert alert-danger"> {{ $message }} </div>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="img">Post IMG</label>
+                            <input type="text" class="form-control" name="img" placeholder="Post IMG" value="{{ old('img') }}">
+                        </div>
+                        <div class="form-group">
+                            <label for="body">Textarea</label>
+                            <textarea class="form-control" name="body" rows="3">{{ (!empty(old('body'))) ? old('body') : 'Inserisci un testo' }}</textarea>
+                            @error('body')
+                            <div class="alert alert-danger"> {{ $message }} </div>
+                            @enderror
+                        </div>
+                        <div class="text-center form-published">
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="published" id="inlineRadio1" value="1" {{ (old('published') == 1) ? 'checked' : '' }}>
+                                <label class="form-check-label" for="published">Published</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="published" id="inlineRadio2" value="0" {{ (old('published') == 0) ? 'checked' : '' }}>
+                                <label class="form-check-label" for="unpublished">Unpublished</label>
+                            </div>
+                        </div>
+                        <div class="text-center">
+                            <button class="btn btn-primary" type="submit" value="salva">Submit form</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+@endsection
+
+@section('footer')
+    <div class="container">
+        <div class="row">
+            <div class="col-4"></div>
+            <div class="col-4">
+                <a href="{{route('posts.published')}}" class="btn btn-dark btn-lg btn-block active" role="button" aria-pressed="true">PUBLISHED POSTS</a>
+                <a href="{{route('posts.index')}}" class="btn btn-light btn-lg btn-block active" role="button" aria-pressed="true">HOME</a>
+            </div>
+            <div class="col-4"></div>
+        </div>
+    </div>
+@endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,12 +4,22 @@
     Post
 @endsection
 
+@section('header')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Post ID: <span class="red-italic">{{$post->id}}</span></h1>
+            </div>
+        </div>
+    </div>
+@endsection
+
 @section('main')
     <body>
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <h1>{{$post->title}}</h1>
+                    <h2>{{$post->title}}</h2>
                     <small>Scritto da <span class="font-italic">{{$post->author}}</span></small>
                     <div class="text-center">
                         <img src="{{$post->img}}" alt="{{$post->title}}">
@@ -30,6 +40,12 @@
             <div class="col-4">
                 <a href="{{route('posts.create')}}" class="btn btn-success btn-lg btn-block active" role="button" aria-pressed="true">CREATE NEW POST</a>
                 <a href="{{route('posts.edit', $post->id)}}" class="btn btn-info btn-lg btn-block active" role="button" aria-pressed="true">EDIT</a>
+                <form action="{{route('posts.destroy', $post->id)}}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger btn-lg btn-block" type="submit">DELETE</button>
+                </form>
+                <a href="{{route('posts.published')}}" class="btn btn-dark btn-lg btn-block active" role="button" aria-pressed="true">PUBLISHED POSTS</a>
                 <a href="{{route('posts.index')}}" class="btn btn-light btn-lg btn-block active" role="button" aria-pressed="true">HOME</a>
             </div>
             <div class="col-4"></div>
